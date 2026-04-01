3fbbb92bfa2f07e88fb11188f30261c58f4e8c00
Loganathan-7264 <sathishloga7264@gmail.com>
2026-04-01 11:26:33 +0530

    first commit

---
+++ MyScript.js
@@ -0,0 +1,46 @@
+// console.log("Hello Loga");
+// var a = 10;
+// console.log("before declaration of a: "+a);
+// var a = 20;
+// console.log("After declaration of a: "+a);
+
+// let b = 20;
+// console.log("before declaration of b:" +b); 
+// b = 30;
+// console.log("After declaration of b:" + b);
+
+// const c = 50;
+// console.log("Before declaration of C:"+c);
+// c = 10;
+// console.log("After declaration of C:"+ c);
+
+let a = "Sathish";
+console.log(a);
+console.log(typeof(a));
+let b = 100;
+console.log(b);
+console.log(typeof(b));
+let c = 34.555;
+console.log(c);
+console.log(typeof(c));
+let d = true;
+console.log(d);
+console.log(typeof(d));
+let e;
+console.log(e);
+console.log(typeof(e));
+let f = null;
+console.log(f);
+console.log(typeof(f));
+
+number = 100+200;
+my_Name= "Sathish"
+console.log(typeof(number), number);
+console.log(typeof(my_Name), my_Name);
+
+let number_A = 10;
+let number_B = 20;
+
+console.log(number_A+number_B);
+let loga = [number_A+number_B+"Tony"];
+console.log(typeof(loga), loga);
