a8aa0b2501e894244e2306724cdcba9ef4fe7b5b
Loganathan-7264 <sathishloga7264@gmail.com>
2026-04-01 11:40:32 +0530

    Arithmetic operations

---
+++ Arithmetic_Operators.js
@@ -0,0 +1,11 @@
+//Input data
+let num1 = 22;
+let num2 = 10;
+
+//Arithmetic Operations
+
+console.log("Addition of num1 & num2: ", num1+num2);
+console.log("Subtraction of num1 & num2: ", num1-num2);
+console.log("Multiplicatin of num1 & num2: ", num1*num2);
+console.log("Division of num1 & num2: ", num1/num2);
+console.log("Modulus of num1 & num2: ", num1%num2);
