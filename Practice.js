6459216e307f5e17fc3e2e3a3767769e99aa856d
Loganathan P <52126070+Loganathan-7264@users.noreply.github.com>
2026-04-01 23:07:52 +0530

    Practice code file for quick check

---
+++ Practice.js
@@ -0,0 +1,20 @@
+// Sample 1
+
+// let a = 10;
+// let b =3;
+// console.log("Sum of a & b: ",a+b);
+// console.log("Differene of a & b: ", a-b);
+// console.log("Product of a & b: ", a*b);
+// console.log("Quotient of a & b: ", a/b);
+
+// sample 2
+
+// let num =7;
+// num = num + 5;
+// console.log(num*2)
+
+// sample 3
+let Price = 50;
+let Tax = (Price*8)/100
+Price = Price + Tax
+console.log(Price)
