a8dc7af9b390c1505c3984cc31e95666edcb6097
Loganathan-7264 <sathishloga7264@gmail.com>
2026-04-03 00:45:14 +0530

    Day 2 assignment

---
+++ Day2/Assignment1.js
@@ -0,0 +1,17 @@
+let studentName = "Loga"; //name
+let studentId = 9876; //id
+
+//marks
+let sub1 = 95;
+let sub2 = 57;
+let sub3 = 65;
+let sub4 = 99;
+let sub5 = 92;
+let totalMarks = sub1+sub2+sub3+sub4+sub5
+let percentage = (totalMarks/500)*100
+
+console.log("Student name: ", studentName);
+console.log("Studenet ID: ", studentId);
+console.log("Total marks: ", totalMarks);
+console.log("Percentage: ", percentage+"%");
+
--- Practice.js
+++ Practice.js
@@ -1,20 +1,21 @@
-// Sample 1
+// // Sample 1
 
-// let a = 10;
-// let b =3;
-// console.log("Sum of a & b: ",a+b);
-// console.log("Differene of a & b: ", a-b);
-// console.log("Product of a & b: ", a*b);
-// console.log("Quotient of a & b: ", a/b);
+// // let a = 10;
+// // let b =3;
+// // console.log("Sum of a & b: ",a+b);
+// // console.log("Differene of a & b: ", a-b);
+// // console.log("Product of a & b: ", a*b);
+// // console.log("Quotient of a & b: ", a/b);
 
-// sample 2
+// // sample 2
 
-// let num =7;
-// num = num + 5;
-// console.log(num*2)
+// // let num =7;
+// // num = num + 5;
+// // console.log(num*2)
 
-// sample 3
-let Price = 50;
-let Tax = (Price*8)/100
-Price = Price + Tax
-console.log(Price)
+// // sample 3
+// let Price = 50;
+// let Tax = (Price*8)/100
+// Price = Price + Tax
+// console.log(Price)
+
