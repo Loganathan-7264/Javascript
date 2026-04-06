147526656edf5268ee359db23d14e2fa45a3fff3
Loganathan-7264 <sathishloga7264@gmail.com>
2026-04-07 00:26:20 +0530

    Practicing

--- Day3/UserDefined.js
+++ Day3/UserDefined.js
@@ -1,4 +1,5 @@
 const reqLine = require("readline-sync");
 // let number = reqLine.questionInt("Enter the number: ");
 let anything = reqLine.question("Enter anything: ");
+reqLine.qu
 console.log(anything);
--- Practice.js
+++ Practice.js
@@ -18,22 +18,59 @@
 // let Tax = (Price*8)/100
 // Price = Price + Tax
 // console.log(Price)
-
+// -----------------------------------------------------------------------------
 // ex 1:
 // let priceString = "50";
 // let priceNumber = Number(priceString);
 // console.log(typeof(priceString));
 // console.log(typeof(priceNumber));
-
+// -----------------------------------------------------------------------------
 // ex2:
 // let isLoggedIn = true;
 // let isAdmin = true;
 // console.log("Delete User button is visible: ", (isLoggedIn && isAdmin));
 
-let totalItems = 10;
+// let totalItems = 10;
 
-if (totalItems = 0) { 
-    console.log("Your cart is empty!");
-} else {
-    console.log("Proceed to checkout");
+// if (totalItems = 0) { 
+//     console.log("Your cart is empty!");
+// } else {
+//     console.log("Proceed to checkout");
+// }
+// -----------------------------------------------------------------------------
+// ODD or Even
+
+// const reqLine = require("readline-sync");
+// let num = reqLine.questionInt("Enter the number: ");
+// let result = (num%2==0)?"Even": "Odd"
+// console.log(result);
+// -----------------------------------------------------------------------------
+// The Grade Calculator
+
+// const reqLine = require("readline-sync");
+// let score = reqLine.questionInt("Enter your score: ");
+
+// if (score>=90 && score<=100){
+//     console.log("Grade A");
+// } else if (score>=80 && score<=89){
+//     console.log("Grade B");
+// }else if (score>=70 && score<=79){
+//     console.log("Grade C");
+// }else if (score<=70 && score>=0){
+//     console.log("Fail");
+// }else{
+//     console.log("Invalid score")
+// }
+
+// ----------------------------------------------------------------------
+// The "Discount Ninja"
+
+const reqLine = require("readline-sync");
+let age = reqLine.questionInt("Enter your age: ");
+let MC = reqLine.question("Do you have a Membership Card? (true/false): ");
+
+if (age>=60 || MC==="true"){
+    console.log("Discount Applied!");
+}else{
+    console.log("Full price!");
 }
